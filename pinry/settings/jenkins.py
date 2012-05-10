6cf61f403889936deb3955b5995df1e13dcd0baa
Isaac Bythewood <isaac@bythewood.me>
2012-05-10 02:36:25 +0000

    Added jenkins tasks to jenkins config.

--- pinry/settings/jenkins.py
+++ pinry/settings/jenkins.py
@@ -24,3 +24,12 @@ PROJECT_APPS = (
     'pinry.pins',
     'pinry.api',
 )
+
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',
+    'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+)
