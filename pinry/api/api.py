0bbcba4a6fadda14f2314bb7dde13eb4d26a852d
Isaac Bythewood <isaac@bythewood.me>
2012-07-06 20:01:33 +0000

    Started implementation of user auth.

--- pinry/api/api.py
+++ pinry/api/api.py
@@ -1,5 +1,9 @@
 from tastypie.resources import ModelResource
 from tastypie import fields
+from tastypie.authentication import BasicAuthentication
+from tastypie.authorization import DjangoAuthorization
+
+from django.contrib.auth.models import User
 
 from pinry.pins.models import Pin
 
@@ -15,3 +19,13 @@ class PinResource(ModelResource):  # pylint: disable-msg=R0904
     def dehydrate_thumbnail(self, bundle):
         pin = Pin.objects.only('image').get(pk=bundle.data['id'])
         return pin.image.url_200x1000
+
+
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'auth/user'
+        excludes = ['email', 'password', 'is_superuser']
+        # Add it here.
+        authentication = BasicAuthentication()
+        authorization = DjangoAuthorization()
--- pinry/api/urls.py
+++ pinry/api/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import patterns, include, url
+
 from .api import PinResource
+from .api import UserResource
+
 
 pin_resource = PinResource()
+user_resource = UserResource()
+
 
 urlpatterns = patterns('',
     url(r'', include(pin_resource.urls)),
+    url(r'', include(user_resource.urls)),
 )
