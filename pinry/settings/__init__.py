e467a83683e1dd973268e58467f5b772dbddbfbc
Isaac Bythewood <isaac@bythewood.me>
2013-02-20 05:35:10 +0000

    Remove admin which was a temporary fix

--- pinry/settings/__init__.py
+++ pinry/settings/__init__.py
@@ -71,7 +71,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.admin',
     'south',
     'compressor',
     'taggit',
--- pinry/urls.py
+++ pinry/urls.py
@@ -1,14 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib import admin
-
-
-admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('pinry.api.urls', namespace='api')),
     url(r'^pins/', include('pinry.pins.urls', namespace='pins')),
     url(r'', include('pinry.core.urls', namespace='core')),
