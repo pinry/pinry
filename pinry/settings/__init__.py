c7e1e1cd83abb6cd6b3e4c89bd45c714b571b53e
Isaac Bythewood <isaac@bythewood.me>
2013-03-03 22:50:29 +0000

    Move location of users middleware

--- pinry/settings/__init__.py
+++ pinry/settings/__init__.py
@@ -44,7 +44,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'pinry.core.middleware.Public',
+    'pinry.users.middleware.Public',
 )
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
