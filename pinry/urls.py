c267b55d0ccf5d77074ea03f115cec9ff6af6b68
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2013-03-05 14:23:10 -0800

    Serve /media/ using Django while in Development
    staticfiles_urlpatterns() add only handler for serving /static/ path,
    add pattern for /media/.

--- pinry/urls.py
+++ pinry/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -6,4 +7,11 @@ urlpatterns = patterns('',
                        url(r'', include('pinry.core.urls', namespace='core')),
                        url(r'', include('pinry.users.urls', namespace='users')),
 )
-urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += patterns('',
+                            url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+                                'document_root': settings.MEDIA_ROOT,
+                                }),
+                            )
