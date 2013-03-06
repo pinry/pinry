b0d1cb4e19ca34b623aeaafcef984e4eb5751a34
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2013-03-06 15:24:51 -0800

    Create a skeleton for documentation

---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+Welcome to Pinry's documentation!
+=================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- setup.py
+++ setup.py
@@ -1,21 +1,44 @@
 import os
 from setuptools import setup, find_packages
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
+dependency_links = [
+# Not released version, PIL dependency replaced by pillow
+'http://github.com/mirumee/django-images/tarball/master#egg=django-images-2013.1.99',
+# Original project has been dead for years, this branch adds Django 1.4 support
+'http://github.com/hcarvalhoalves/django-taggit/tarball/master#egg=django-taggit-0.9.3.99'
+]
+
+install_requires = [
+    'Django>=1.5',
+    'Pillow'
+    'South'
+    'django-tastypie'
+    'django-braces'
+    'django_compressor'
+    'django-images>=2013.1.99',
+    'django-taggit>=0.9.3.99'
+]
+
 setup(
-    name = "pinry",
-    version = "0.9.0",
-    author = "Pinry contributors",
-    author_email = "devs@getpinry.com",
-    description = ("A tiling image board system for people who want to save, "
-                   "tag, and share images, videos and webpages."),
-    license = "AGPL-3+",
-    keywords = "web pictures kittens",
-    url = "http://packages.python.org/pinry",
+    name="pinry",
+    version="0.9.0",
+    author="Pinry contributors",
+    author_email="devs@getpinry.com",
+    description=("A tiling image board system for people who want to save, "
+                 "tag, and share images, videos and webpages."),
+    license="AGPL-3+",
+    keywords="web pictures kittens",
+    url="http://packages.python.org/pinry",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     long_description=read('README.md'),
+    dependency_links=dependency_links,
+    tests_require=['mock', 'factory-boy'],
+    install_requires=install_requires,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Framework :: Django",
