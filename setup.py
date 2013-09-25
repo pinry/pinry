0a9a27706517c05f2f1cb3fdd1ebf5d11c1c1d8e
Isaac Bythewood <isaac@bythewood.me>
2013-09-25 23:21:10 +0000

    Update setup to most recent version and packages

--- setup.py
+++ setup.py
@@ -6,45 +6,38 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-dependency_links = [
-# Not released version, PIL dependency replaced by pillow
-'http://github.com/kklimonda/django-images/tarball/master#egg=django-images-2013.1.99',
-# Original project has been dead for years, this branch adds Django 1.4 support
-'http://github.com/hcarvalhoalves/django-taggit/tarball/master#egg=django-taggit-0.9.3.99'
-]
-
 install_requires = [
-    'Django>=1.5',
+    'Django',
     'Pillow',
     'South',
-    'django-tastypie==0.9.12',
+    'requests',
+    'django-taggit',
+    'django-images',
     'django-braces',
     'django_compressor',
-    'requests',
-    'django-images',
-    'django-taggit'
+    'django-tastypie==0.9.14',
 ]
 
 setup(
-    name="pinry",
-    version="1.1.0",
-    author="Pinry contributors",
+    name="Pinry",
+    version="1.3.1",
+    author="Pinry Contributors",
     author_email="devs@getpinry.com",
     description=("A tiling image board system for people who want to save, "
                  "tag, and share images, videos and webpages."),
     license="AGPL-3+",
-    keywords="web pictures kittens",
-    url="http://packages.python.org/pinry",
+    keywords="django tiling board tag share images pictures videos webpages",
+    url="http://getpinry.com/",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     long_description=read('README.md'),
     dependency_links=dependency_links,
     tests_require=['mock', 'factory-boy>=1.3,<2.0'],
     install_requires=install_requires,
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: BSD License",
         "Framework :: Django",
         "Environment :: Web Environment",
-        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
     ],
 )
 
