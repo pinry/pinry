813ab93c11cd54cf52cfe83c1b892e3bbeff1fa7
Isaac Bythewood <isaac@bythewood.me>
2017-02-11 05:41:24 +0000

    Change default of user registrations to True since it is a common problem with new users.

--- pinry/settings.py
+++ pinry/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/static/media/'
 
 # Set to False to disable people from creating new accounts.
-ALLOW_NEW_REGISTRATIONS = False
+ALLOW_NEW_REGISTRATIONS = True
 
 # Set to False to force users to login before seeing any pins.
 PUBLIC = True
