2d8d92b8b7cacc21a5fbbd124c3ede6423dd8b01
Isaac Bythewood <isaac@bythewood.me>
2017-02-11 04:32:06 +0000

    Add template_settings back to context processors

--- pinry/settings.py
+++ pinry/settings.py
@@ -53,6 +53,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'pinry.core.context_processors.template_settings',
             ],
         },
     },
