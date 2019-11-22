d2985415022fda1b01f7f5a4849fb468bcf74bbb
winkidney <winkidney@gmail.com>
2019-11-22 19:12:02 +0800

    Feature: Add brikcs js on home page

--- pinry-spa/src/main.js
+++ pinry-spa/src/main.js
@@ -1,9 +1,12 @@
 import Buefy from 'buefy';
+import 'vue-bricks/lib/vueBricks.css';
+import VueBricks from 'vue-bricks';
 import Vue from 'vue';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
 Vue.use(Buefy);
+Vue.use(VueBricks);
 
 new Vue({
   render: h => h(App),
