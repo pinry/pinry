c8f3dadbf7593efca41c2e0c69f21bda1d7dca7e
winkidney <winkidney@gmail.com>
2019-11-22 17:38:40 +0800

    Feature: Turn off line-length test for eslint

--- pinry-spa/.eslintrc.js
+++ pinry-spa/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'max-len': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
