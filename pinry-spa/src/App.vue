d31406e4258d1228f477e3213a5c5c878da58137
winkidney <winkidney@gmail.com>
2019-11-22 16:39:36 +0800

    Feature: Add buefy as default theme tools

--- pinry-spa/src/App.vue
+++ pinry-spa/src/App.vue
@@ -17,12 +17,9 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  // Import Bulma's core
+  @import "~bulma/sass/utilities/_all";
+  // Import Bulma and Buefy styles
+  @import "~bulma";
+  @import "~buefy/src/scss/buefy";
 </style>
