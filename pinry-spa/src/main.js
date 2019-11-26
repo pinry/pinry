707825769b0ca419ad142bedf29514e9de287c22
winkidney <winkidney@gmail.com>
2019-11-26 23:43:46 +0800

    Feature: Add vue-router as default function

--- pinry-spa/src/main.js
+++ pinry-spa/src/main.js
@@ -2,6 +2,7 @@ import Buefy from 'buefy';
 import Vue from 'vue';
 import { VueMasonryPlugin } from 'vue-masonry';
 import App from './App.vue';
+import router from './router';
 
 
 Vue.config.productionTip = false;
@@ -9,5 +10,6 @@ Vue.use(Buefy);
 Vue.use(VueMasonryPlugin);
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app');
