cd368e939bc0c684e45c0eead636ff9394da47d0
winkidney <winkidney@gmail.com>
2019-11-26 23:43:46 +0800

    Feature: Add vue-router as default function

--- pinry-spa/src/App.vue
+++ pinry-spa/src/App.vue
@@ -1,17 +1,12 @@
 <template>
   <div id="app">
-    <Home></Home>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Home from './pages/Home.vue';
-
 export default {
   name: 'app',
-  components: {
-    Home,
-  },
 };
 </script>
 
