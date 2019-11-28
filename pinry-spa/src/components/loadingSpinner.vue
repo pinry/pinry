5890a0280769aea22e0694048a83134f2a7dfe8b
winkidney <winkidney@gmail.com>
2019-11-28 23:38:50 +0800

    Feature: Add loading spinner for pins

---
+++ pinry-spa/src/components/loadingSpinner.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="loading-spinner">
+    <div v-show="show" class="spinner">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'loadingSpinner',
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+/* loading spinner */
+.spinner {
+    background: url('../assets/loader.gif');
+    background-position: center center;
+    background-repeat: no-repeat;
+    height: 31px;
+    margin: 0 auto;
+    padding: 50px;
+    width: 31px;
+}
+</style>
