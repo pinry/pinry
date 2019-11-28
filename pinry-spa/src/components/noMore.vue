a6d3341edd1bf6086ccff41c462514f85d4a217c
winkidney <winkidney@gmail.com>
2019-11-29 00:22:42 +0800

    Feature: Add "end" display at the end of page

---
+++ pinry-spa/src/components/noMore.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-show="show">
+    <div id="the-end">&mdash; End &mdash;</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'noMore',
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#the-end {
+    font-family: 'Georgia', 'Times', 'Times New Roman', serif;
+    font-size: 1.4em;
+    font-style: italic;
+    text-align: center;
+    padding: 50px;
+}
+</style>
