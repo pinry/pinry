0006aad6f7aef98cdeae6ad8ab5f172d3e1c7317
winkidney <winkidney@gmail.com>
2019-12-04 23:44:44 +0800

    Feature: Create Pin from uploaded file

--- pinry-spa/src/components/pin_create/FileUpload.vue
+++ pinry-spa/src/components/pin_create/FileUpload.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="image-upload">
     <div
-      v-show="previewExists"
+      v-show="previewImage !== null"
       class="has-text-centered is-center preview">
-      <img :src="previewImageURL">
+      <img :src="previewImage">
     </div>
-    <div v-show="!previewExists">
+    <div v-show="previewImage === null">
       <b-field>
-        <b-upload v-model="dropFiles"
-                  multiple
+        <b-upload v-model="dropFile"
+                  :loading="loading"
                   drag-drop>
           <section class="section">
             <div class="content has-text-centered">
@@ -28,25 +28,57 @@
 </template>
 
 <script>
+import API from '../api';
+import utils from '../utils/PinHandler';
+
 export default {
   name: 'FileUpload',
+  data() {
+    return {
+      dropFile: null,
+      loading: false,
+      uploadedImage: null,
+    };
+  },
   props: {
     previewImageURL: {
       type: String,
       default: null,
     },
   },
+  watch: {
+    dropFile(newFile) {
+      this.$emit('imageUploadProcessing');
+      this.loading = true;
+      API.Pin.uploadImage(newFile).then(
+        (resp) => {
+          this.uploadedImage = resp.data;
+          this.loading = false;
+          this.$emit('imageUploadSucceed', this.uploadedImage.id);
+        },
+        () => {
+          this.loading = false;
+          this.$emit('imageUploadFailed');
+        },
+      );
+    },
+  },
   computed: {
+    previewImage() {
+      if (this.previewExists()) {
+        return this.previewImageURL;
+      }
+      if (this.uploadedImage !== null) {
+        return utils.escapeUrl(this.uploadedImage.thumbnail.image);
+      }
+      return null;
+    },
+  },
+  methods: {
     previewExists() {
       return this.previewImageURL !== null && this.previewImageURL !== '';
     },
   },
-  data() {
-    return {
-      dropFiles: [],
-    };
-  },
-  methods: {},
 };
 </script>
 
