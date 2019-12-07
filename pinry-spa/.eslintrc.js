23d48a7a0f818584f7fb895f33eef2400df99811
winkidney <winkidney@gmail.com>
2019-12-08 01:26:18 +0800

    Feature: Allow a little console.log for production

--- pinry-spa/.eslintrc.js
+++ pinry-spa/.eslintrc.js
@@ -8,9 +8,10 @@ module.exports = {
     '@vue/airbnb',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'max-len': 'off',
+    "no-console": "off",
   },
   parserOptions: {
     parser: 'babel-eslint',
