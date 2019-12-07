f54ccf7c2ef7e1abfb9f7d5f2ff2f53b92e39d8e
winkidney <winkidney@gmail.com>
2019-12-07 13:09:22 +0800

    Feature: Add buttons for pin-edit

---
+++ pinry-spa/src/components/EditorUI.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="editor">
+    <div class="editor-buttons">
+      <span class="icon-container">
+          <b-icon
+            type="is-light"
+            icon="heart"
+            custom-size="mdi-24px">
+         </b-icon>
+      </span>
+      <span class="icon-container">
+         <b-icon
+           type="is-light"
+           icon="delete"
+           custom-size="mdi-24px">
+         </b-icon>
+      </span>
+      <span class="icon-container">
+       <b-icon
+         type="is-light"
+         icon="pencil"
+         custom-size="mdi-24px">
+       </b-icon>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Editor',
+  props: {
+    show: {
+      default: false,
+      type: Boolean,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@mixin border-radius {
+  border-radius: 3px;
+}
+
+.editor-buttons {
+  padding: 3px 1px 3px 3px;
+  background-color: rgba(225, 225, 225, 0.6);
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  float: right;
+  cursor: pointer;
+
+  @include border-radius;
+  .icon-container {
+    @include border-radius;
+    padding: 5px 2px 2px 2px;
+    background-color: rgba(0, 0, 0, 0.8);
+    margin-right: 2px;
+    span {
+      opacity: 0.9;
+    }
+  }
+}
+</style>
