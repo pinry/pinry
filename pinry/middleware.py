7608ef928aa22f207be868a9d24c6ece8544dab3
winkidney <winkidney@gmail.com>
2020-07-17 13:42:35 +0800

    Fix: Try to migrate middlware to new version

--- pinry/middleware.py
+++ pinry/middleware.py
@@ -2,6 +2,10 @@ from django.middleware.csrf import get_token
 
 
 class ForceCSRFCookieMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
     def process_request(self, request):
         if "CSRF_TOKEN" not in request.META:
             get_token(request)
--- users/middleware.py
+++ users/middleware.py
@@ -2,11 +2,14 @@ from django.conf import settings
 from django.http import HttpResponseForbidden
 
 
-class Public(object):
+class Public:
 
     acceptable_paths = (
         "/api/v2/profile/",
     )
+
+    def __init__(self, get_response):
+        self.get_response = get_response
 
     def process_request(self, request):
         if settings.PUBLIC is False and not request.user.is_authenticated():
