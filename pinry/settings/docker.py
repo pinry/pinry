9a6088f0b1edbee311cc9529883f76bc6f97bb01
Alexandra Dunn <dunn.alex@gmail.com>
2021-01-21 21:28:55 -0800

    docker: configure more settings via env var, ignore local_settings

--- pinry/settings/docker.py
+++ pinry/settings/docker.py
@@ -1,18 +1,45 @@
+from distutils import util
 import logging
 
 from .base import *
 
-
-# SECURITY WARNING: keep the secret key used in production secret!
-if 'SECRET_KEY' not in os.environ:
-    logging.warning(
-        "No SECRET_KEY given in environ, please have a check."
-        "If you have a local_settings file, please ignore this warning."
-    )
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 SECRET_KEY = os.environ.get('SECRET_KEY')
+STATIC_ROOT = '/data/static'
+USE_X_FORWARDED_HOST = True
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+ALLOW_NEW_REGISTRATIONS = bool(util.strtobool(
+    os.environ.get('ALLOW_NEW_REGISTRATIONS', 'True')
+))
+DEBUG = bool(util.strtobool(
+    os.environ.get('DEBUG', 'False')
+))
+IMAGE_AUTO_DELETE = bool(util.strtobool(
+    os.environ.get('IMAGE_AUTO_DELETE', 'True')
+))
+PUBLIC = bool(util.strtobool(
+    os.environ.get('PUBLIC_PINS', 'True')
+))
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'PORT': 5432,
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+    }
+}
+
+IMAGE_SIZES = {
+    'thumbnail': {'size': [240, 0]},
+    'standard': {'size': [600, 0]},
+    'square': {'crop': True, 'size': [125, 125]},
+}
 
 LOGGING = {
     'version': 1,
@@ -30,26 +57,6 @@ LOGGING = {
     },
 }
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
-        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'PORT': 5432,
-        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-    }
-}
-
-USE_X_FORWARDED_HOST = True
-
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
     'rest_framework.renderers.JSONRenderer',
 ]
-
-# should not ignore import error in production, local_settings is required
-from .local_settings import *
