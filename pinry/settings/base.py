18bda6dfd156ae1e31423e4438c5b262b9a1ce35
winkidney <winkidney@gmail.com>
2022-02-18 21:37:18 -0800

    Fix: Should enable token auth in DRF

--- pinry/settings/base.py
+++ pinry/settings/base.py
@@ -164,4 +164,8 @@ REST_FRAMEWORK = {
     'URL_FIELD_NAME': DRF_URL_FIELD_NAME,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': API_LIMIT_PER_PAGE,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
