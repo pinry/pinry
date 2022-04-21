a8d8cb6eb22bb7ed8e3404d41b1b4a70abfaf78a
winkidney <winkidney@gmail.com>
2022-04-21 08:18:51 -0700

    feature: add git tag to docker build

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -2,13 +2,11 @@
 
 script_dir="$( dirname "${0}" )"
 
-IMAGE_TAG="latest"
+IMAGE_LATEST_TAG="latest"
+IMAGE_VERSION_TAG=`git tag -l --sort=-creatordate | head -n 1 | sed "s/v//g"`
 
-# shellcheck disable=SC2199
-if [[ "${@}" != "" ]];then
-    # shellcheck disable=SC2124
-    IMAGE_TAG="${@}"
-fi
+sudo docker build -t getpinry/pinry:${IMAGE_LATEST_TAG} "${script_dir}/../" \
+  -f "${script_dir}/../Dockerfile.autobuild"
 
-sudo docker build -t getpinry/pinry:${IMAGE_TAG} "${script_dir}/../" \
+sudo docker build -t getpinry/pinry:${IMAGE_VERSION_TAG} "${script_dir}/../" \
   -f "${script_dir}/../Dockerfile.autobuild"
--- docker/build_docker_multiple_platform.sh
+++ docker/build_docker_multiple_platform.sh
@@ -2,16 +2,17 @@
 
 script_dir="$( dirname "${0}" )"
 
-IMAGE_TAG="latest"
-
-# shellcheck disable=SC2199
-if [[ "${@}" != "" ]];then
-    # shellcheck disable=SC2124
-    IMAGE_TAG="${@}"
-fi
+IMAGE_LATEST_TAG="latest"
+IMAGE_VERSION_TAG=`git tag -l --sort=-creatordate | head -n 1 | sed "s/v//g"`
 
 sudo docker buildx build \
   --push \
   --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
-  -t getpinry/pinry:${IMAGE_TAG} "${script_dir}/../" \
+  -t getpinry/pinry:${IMAGE_LATEST_TAG} "${script_dir}/../" \
+  -f "${script_dir}/../Dockerfile.autobuild"
+
+sudo docker buildx build \
+  --push \
+  --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
+  -t getpinry/pinry:${IMAGE_VERSION_TAG} "${script_dir}/../" \
   -f "${script_dir}/../Dockerfile.autobuild"
