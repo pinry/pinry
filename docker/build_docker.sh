8a7cb756dc6b3c562df92a5fc92c2f4e390f4094
winkidney <winkidney@gmail.com>
2022-04-23 06:34:20 -0700

    refactor: simplify docker build scripts

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -5,8 +5,8 @@ script_dir="$( dirname "${0}" )"
 IMAGE_LATEST_TAG="latest"
 IMAGE_VERSION_TAG=`git tag -l --sort=-creatordate | head -n 1 | sed "s/v//g"`
 
-sudo docker build -t getpinry/pinry:${IMAGE_LATEST_TAG} "${script_dir}/../" \
-  -f "${script_dir}/../Dockerfile.autobuild"
-
-sudo docker build -t getpinry/pinry:${IMAGE_VERSION_TAG} "${script_dir}/../" \
+sudo docker build \
+  -t getpinry/pinry:${IMAGE_VERSION_TAG} \
+  -t getpinry/pinry:${IMAGE_LATEST_TAG} \
+  "${script_dir}/../" \
   -f "${script_dir}/../Dockerfile.autobuild"
--- docker/build_docker_multiple_platform.sh
+++ docker/build_docker_multiple_platform.sh
@@ -8,11 +8,7 @@ IMAGE_VERSION_TAG=`git tag -l --sort=-creatordate | head -n 1 | sed "s/v//g"`
 sudo docker buildx build \
   --push \
   --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
-  -t getpinry/pinry:${IMAGE_LATEST_TAG} "${script_dir}/../" \
-  -f "${script_dir}/../Dockerfile.autobuild"
-
-sudo docker buildx build \
-  --push \
-  --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
-  -t getpinry/pinry:${IMAGE_VERSION_TAG} "${script_dir}/../" \
+  -t getpinry/pinry:${IMAGE_LATEST_TAG} \
+  -t getpinry/pinry:${IMAGE_VERSION_TAG} \
+  "${script_dir}/../" \
   -f "${script_dir}/../Dockerfile.autobuild"
