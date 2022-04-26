2e93da1adfb4408aca88975449bfabb9d50dead2
winkidney <winkidney@gmail.com>
2022-04-25 21:18:05 -0700

    fix: remove cache for docker build

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -6,6 +6,7 @@ IMAGE_LATEST_TAG="latest"
 IMAGE_VERSION_TAG=`git tag -l --sort=-creatordate | head -n 1 | sed "s/v//g"`
 
 sudo docker build \
+  --no-cache \
   -t getpinry/pinry:${IMAGE_VERSION_TAG} \
   -t getpinry/pinry:${IMAGE_LATEST_TAG} \
   "${script_dir}/../" \
