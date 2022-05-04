bda7246d309877be396494a4d101af661f921e10
winkidney <winkidney@gmail.com>
2022-05-04 21:54:21 +0800

    feature: take back build scripts for multiple platform

--- docker/build_docker_multiple_platform.sh
+++ docker/build_docker_multiple_platform.sh
@@ -5,19 +5,10 @@ script_dir="$( dirname "${0}" )"
 IMAGE_LATEST_TAG="latest"
 IMAGE_VERSION_TAG=`git tag -l --sort=-creatordate | head -n 1 | sed "s/v//g"`
 
-#sudo docker buildx build \
-#  --no-cache \
-#  --push \
-#  --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
-#  -t getpinry/pinry:${IMAGE_LATEST_TAG} \
-#  -t getpinry/pinry:${IMAGE_VERSION_TAG} \
-#  "${script_dir}/../" \
-#  -f "${script_dir}/../Dockerfile.autobuild"
-
 sudo docker buildx build \
   --no-cache \
-  --output=type=docker \
-  --platform linux/amd64 \
+  --push \
+  --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
   -t getpinry/pinry:${IMAGE_LATEST_TAG} \
   -t getpinry/pinry:${IMAGE_VERSION_TAG} \
   "${script_dir}/../" \
