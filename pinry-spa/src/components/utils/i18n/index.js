e588c8044c91a05dc211d3274a2cd98f67408134
Lilian <lilian@boulard.fr>
2022-09-08 00:34:26 +0200

    Add French translation, v1

--- pinry-spa/src/components/utils/i18n/index.js
+++ pinry-spa/src/components/utils/i18n/index.js
@@ -1,14 +1,17 @@
 import en from './locales/en.json';
 import zh from './locales/zh.json';
+import fr from './locales/fr.json';
 
 const messages = {
   en,
   zh,
+  fr,
 };
 
 const langCode2Name = {
   en: 'English',
   zh: '简体中文',
+  fr: 'Français',
 };
 
 export default {
