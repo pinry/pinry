4793d98401641f3250ea25b394f2dae957684966
Thomas R. Koll <tomk32@tomk32.de>
2025-05-30 07:13:15 +0200

    Allow lint script to be used with just a single file

--- pinry-spa/eslint.config.mjs
+++ pinry-spa/eslint.config.mjs
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 
 export default defineConfig([
@@ -10,5 +10,6 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginVue.configs["flat/essential"],
+  globalIgnores(["dist/", "*.config*js"]),
   { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
 ]);
